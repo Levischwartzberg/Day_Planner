59b12fc397b94427a0622742b95b9b4b121d77d0
Levi Schwartzberg <Levischwartzberg@gmail.com>
2021-03-18 15:43:06 -0500

    added some styling and began work on storing values

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,32 +1,33 @@
-console.log(moment().format('MMMM Do YYYY, h:mm:ss a'));
+var today = moment().format('MMMM Do YYYY');
+var day = $("#currentDay");
+day.text(today);
 
 var times = [["8AM", 8], ["9AM", 9], ["10AM", 10], ["11AM", 11], ["12PM", 12], ["1PM", 13], ["2PM", 14], ["3PM", 15], ["4PM", 16], ["5PM", 17]];
 var containerEl = $(".container");
+var textStorage = [];
 
 for (i=0; i<times.length; i++) {
-    var currentTime = moment().format('LT');
-    console.log(currentTime);
+    var currentHour = moment().format('HH');
+    //console.log(currentTime);
     
-    currentHour = convertTime(currentTime);
+    //currentHour = convertTime(currentTime);
     if (currentHour > parseInt(times[i][1])) {
-        var contentText = $(`<input type="text" class="textarea past" id="plans" name="plans" placeholder="" />`); 
-        console.log(times[i][1] + " " + currentHour);
+        var contentText = $(`<textarea type="text" class="textarea past col-6 col-md-8" index="${i}" id="plans" name="plans" placeholder="" />`); 
+        //console.log(times[i][1] + " " + currentHour);
     }
     else if (currentHour < parseInt(times[i][1])) {
-        var contentText = $(`<input type="text" class="textarea future" id="plans" name="plans" placeholder="" />`); 
-        console.log(times[i][1] + " " + currentHour);
+        var contentText = $(`<textarea type="text" class="textarea future col-6 col-md-8" index="${i}" id="plans" name="plans" placeholder="" />`); 
+        //console.log(times[i][1] + " " + currentHour);
     }
     else if (currentHour == parseInt(times[i][1])) {
-        var contentText = $(`<input type="text" class="textarea present" id="plans" name="plans" placeholder="" />`); 
-        console.log(times[i][1] + " " + currentHour);
+        var contentText = $(`<textarea type="text" class="textarea present col-6 col-md-8" index="${i}" id="plans" name="plans" placeholder="" />`); 
+        //console.log(times[i][1] + " " + currentHour);
     }
 
-    
-
     var hour = $(`<div class="hour"> ${times[i][0]} </div>`);
-    var saveButton = $(`<div class="saveBtn"> </div>`);
+    var saveButton = $(`<div class="saveBtn"> <i class="fas fa-save"> </div>`);
 
-    var row = $(`<div class="row"> </div>`);
+    var row = $(`<div class="row justify-content-center"> </div>`);
     var timeBlock = $(`<div class="time-block"> </div>`);
     row.append(hour);
     row.append(contentText);
@@ -38,16 +39,26 @@ containerEl.on("click", ".saveBtn", saveText);
 
 function saveText(event) {
     var saved = ( $(this).parent().children().eq(1) ).val();
-    console.log( saved );
+    //console.log(saved);
+    var index = $(this).parent().children().eq(1).attr("index");
+    //console.log(index);
+    textStorage[index] = saved;
+    console.log(textStorage);
 }
 
-function convertTime(time) {
-    var hour = time[0] + time[1];
-    var hourNum = parseInt(hour);
-    if (hour[1] = ":") {
-        hour = hour[0];
-        hourNum += 12;
-    } 
-    return hourNum;
-}
+// function convertTime(time) {
+//     var hour = time[0] + time[1];
+//     var hourNum = parseInt(hour);
+//     if (hour[1] = ":") {
+//         if (time[5] == "P") {
+//             hourNum += 12;
+//         }
+//     }
+//     else {
+//         if (time[6] == "P") {
+//             hourNum += 12;
+//         }
+//     }
+//     return hourNum;
+// }
 
