2fa149c2c10839725afcf6a70d1905641dc977dd
Levi Schwartzberg <Levischwartzberg@gmail.com>
2021-03-18 19:47:10 -0500

    added comments

--- assets/scripts.js
+++ assets/scripts.js
@@ -2,32 +2,35 @@ var today = moment().format('MMMM Do YYYY');
 var day = $("#currentDay");
 day.text(today);
 
-var times = [["8AM", 8], ["9AM", 9], ["10AM", 10], ["11AM", 11], ["12PM", 12], ["1PM", 13], ["2PM", 14], ["3PM", 15], ["4PM", 16], ["5PM", 17]];
 var containerEl = $(".container");
+//times array is used to initialize document
+var times = ["8AM","9AM","10AM","11AM","12PM","1PM","2PM","3PM","4PM","5PM"];
+
+//local storage pulls from local storage if it's populated, otherwise it sets it to an empty array that will be populated
 var textStorage = JSON.parse(localStorage.getItem("hour_planner"));
 if (textStorage == null) {
     textStorage = [];
 }
 
+//for loop creates hour rows for each hour in the times array
 for (i=0; i<times.length; i++) {
     var currentHour = moment().format('HH');
-    //console.log(currentTime);
     
-    //currentHour = convertTime(currentTime);
-    if (currentHour > parseInt(times[i][1])) {
+    //logic uses moment time to determine the class of the divs to influence the color
+    if (currentHour > parseInt(times[i])) {
         var contentText = $(`<textarea type="text" class="textarea past col-6 col-md-8" index="${i}" id="plans" name="plans" placeholder="" />`);
         contentText.text(textStorage[i]);
     }
-    else if (currentHour < parseInt(times[i][1])) {
+    else if (currentHour < parseInt(times[i])) {
         var contentText = $(`<textarea type="text" class="textarea future col-6 col-md-8" index="${i}" id="plans" name="plans" placeholder="" />`);
         contentText.text(textStorage[i]);
     }
-    else if (currentHour == parseInt(times[i][1])) {
+    else if (currentHour == parseInt(times[i])) {
         var contentText = $(`<textarea type="text" class="textarea present col-6 col-md-8" index="${i}" id="plans" name="plans" placeholder="" />`); 
         contentText.text(textStorage[i]);
     }
 
-    var hour = $(`<div class="hour"> ${times[i][0]} </div>`);
+    var hour = $(`<div class="hour"> ${times[i]} </div>`);
     var saveButton = $(`<div class="saveBtn"> <i class="fas fa-save"> </div>`);
 
     var row = $(`<div class="row justify-content-center"> </div>`);
@@ -38,8 +41,10 @@ for (i=0; i<times.length; i++) {
     containerEl.append(row);
 }
 
+//event listener for all save buttons
 containerEl.on("click", ".saveBtn", saveText);
 
+//function called on save button click grabs the index and text value from the specific field and adds it to the text storage array
 function saveText(event) {
     var saved = ( $(this).parent().children().eq(1) ).val();
     //console.log(saved);
@@ -49,20 +54,3 @@ function saveText(event) {
     //console.log(textStorage);
     localStorage.setItem("hour_planner", JSON.stringify(textStorage));
 }
-
-// function convertTime(time) {
-//     var hour = time[0] + time[1];
-//     var hourNum = parseInt(hour);
-//     if (hour[1] = ":") {
-//         if (time[5] == "P") {
-//             hourNum += 12;
-//         }
-//     }
-//     else {
-//         if (time[6] == "P") {
-//             hourNum += 12;
-//         }
-//     }
-//     return hourNum;
-// }
-
